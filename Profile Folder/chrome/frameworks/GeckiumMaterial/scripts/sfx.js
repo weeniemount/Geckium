917de70f4bbadc8eedd8050f3c9eb58bc88d03b6
angelbruni <brunooshirof@protonmail.com>
2024-11-19 23:00:14 +0000

    Fixed tick sound path being hardcoded

--- Profile Folder/chrome/frameworks/GeckiumMaterial/scripts/sfx.js
+++ Profile Folder/chrome/frameworks/GeckiumMaterial/scripts/sfx.js
@@ -1,5 +1,5 @@
 /* I shouldn't do the sound effect like this, but the GeckiumMaterial code is bad, so this will do for now */
-const SFXTick = new Audio("file:///home/brunof/.mozilla/firefox/capy8v0g.default-release/chrome/frameworks/GeckiumMaterial/sounds/effects/ogg/Effect_Tick.ogg");
+const SFXTick = new Audio("chrome://userchrome/content/frameworks/GeckiumMaterial/sounds/effects/ogg/Effect_Tick.ogg");
 setTimeout(() => {
 	document.querySelectorAll('button, label.button, .input, .checkbox-parent, .switch-parent, .radio-parent, .step').forEach(btn => {
 		btn.addEventListener("mousedown", () => {

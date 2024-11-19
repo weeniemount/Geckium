097a90953f910f203ea07b32afd483630648e0a1
angelbruni <brunooshirof@protonmail.com>
2024-11-19 16:27:29 +0000

    Added tick SFX to GeckiumMaterial

---
+++ Profile Folder/chrome/frameworks/GeckiumMaterial/scripts/sfx.js
@@ -0,0 +1,13 @@
+/* I shouldn't do the sound effect like this, but the GeckiumMaterial code is bad, so this will do for now */
+const SFXTick = new Audio("file:///home/brunof/.mozilla/firefox/capy8v0g.default-release/chrome/frameworks/GeckiumMaterial/sounds/effects/ogg/Effect_Tick.ogg");
+setTimeout(() => {
+	document.querySelectorAll('button, label.button, .input, .checkbox-parent, .switch-parent, .radio-parent').forEach(btn => {
+		btn.addEventListener("mousedown", () => {
+			const snd = SFXTick.cloneNode();
+			snd.play();
+			snd.addEventListener("ended", () => {
+				snd.remove();
+			});
+		})
+	})
+}, 100);

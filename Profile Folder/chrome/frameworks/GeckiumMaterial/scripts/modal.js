bcb09064c5645eb18c04e7dfdd88ee24986a9d82
angelbruni <brunooshirof@protonmail.com>
2024-12-24 18:27:28 +0000

    Made it so multiple modals can be toggled at once

--- Profile Folder/chrome/frameworks/GeckiumMaterial/scripts/modal.js
+++ Profile Folder/chrome/frameworks/GeckiumMaterial/scripts/modal.js
@@ -1,25 +1,26 @@
-let modalToggles = document.querySelectorAll('[data-toggle-modal]');
+let modalToggles = document.querySelectorAll('[data-toggle-modals]');
 let modals = document.querySelectorAll('[data-modal]');
 let mainWindow = document.querySelector('#window');
 
 modalToggles.forEach(modalToggle => {
 	modalToggle.addEventListener('click', function() {
-		let modalId = modalToggle.dataset.toggleModal;
-		let modalTarget = document.querySelector(`[data-modal="${modalId}"]`);
+		let modalToggleIds = modalToggle.dataset.toggleModals.replace(" ", "").split(",");
 
-		if (modalTarget.classList.contains('active'))
-			modalTarget.classList.remove('active');
-		else
-			modalTarget.classList.add('active');
-	});
+		modalToggleIds.forEach(id => {
+			let target = document.querySelector(`[data-modal="${id}"]`);
+			if (target.classList.contains('active'))
+				target.classList.remove('active');
+			else
+				target.classList.add('active');
+		});
+	}); 	
 });
 
 modals.forEach(modal => {
     modal.addEventListener('click', function(event) {
         if (!event.target.closest('.card')) {
-            if (modal.classList.contains('active')) {
-              modal.classList.remove('active');
-            }
+            if (modal.classList.contains('active'))
+              	modal.classList.remove('active');
         }
     });
 });
